47fbae9a6e1356789377536f643106fdf6af9f02
SAD DUST <90087346+SAD-DUST@users.noreply.github.com>
2026-01-31 16:34:53 +0700

    Update meta.js

--- meta.js
+++ meta.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         OLM Suite
 // @namespace    http://tampermonkey.net/
-// @version      1.6
+// @version      1.7
 // @description  Nothing else
 // @author       SAD_DUST
 // @match        https://olm.vn/*
-// @updateURL    https://your-repo/olm-suite.meta.js
-// @downloadURL  https://your-repo/olm-suite.user.js
+// @updateURL    https://raw.githubusercontent.com/SAD-DUST/share-file/refs/heads/main/meta.js
+// @downloadURL  https://raw.githubusercontent.com/SAD-DUST/share-file/refs/heads/main/olm.js
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
