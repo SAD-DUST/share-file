f4d6a719e3c21df48cca2296eeeca3ea3bbf59c1
SAD DUST <90087346+SAD-DUST@users.noreply.github.com>
2026-01-31 16:29:32 +0700

    Add initial UserScript metadata for OLM Suite

---
+++ meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         OLM Suite
+// @namespace    http://tampermonkey.net/
+// @version      1.6
+// @description  Nothing else
+// @author       SAD_DUST
+// @match        https://olm.vn/*
+// @updateURL    https://your-repo/olm-suite.meta.js
+// @downloadURL  https://your-repo/olm-suite.user.js
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
